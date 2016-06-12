f70f78df3c44c87752b84103ef0a608039812756
hhkk <hank.khang@gmail.com>
2016-06-12 02:59:54 -0400

    up to demo step 7 I guess

---
+++ README_HK.txt
@@ -0,0 +1,1 @@
+at this level F:\Users\henryms\Downloads\meteor-angular2.0-socially-step_07\meteor-angular2.0-socially-step_07
--- client/app.ts
+++ client/app.ts
@@ -1,68 +1,20 @@
 import 'reflect-metadata';
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-// see meteor npm install angular2-meteor-auto-bootstrap --save
-// on https://www.angular-meteor.com/tutorials/socially/angular2/3-way-data-binding
-// http://guide.meteor.com/using-npm-packages.html (about --save)
+import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
-//old import { bootstrap } from '@angular/platform-browser-dynamic';
-
-import { Parties }   from '../collections/parties';
-import { Mongo }     from 'meteor/mongo';
+import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from '@angular/router-deprecated';
+import { APP_BASE_HREF } from '@angular/common';
+import { PartiesListxxx } from './imports/parties-list/parties-list.ts';
+import { PartyDetails } from './imports/party-details/party-details.ts';
 
 @Component({
-    selector: 'app2x',      // hijack this tag in index.html        hhkk
-    templateUrl: 'client/app4.html' // and put this in its place
+  selector: 'app',
+  templateUrl: '/client/app.html',
+  directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  { path: '/', as: 'PartiesListxxx', component: PartiesListxxx },
+  { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails }
+])
+class Socially {}
 
-class Socially {
-    partiesxx: Mongo.Cursor<Object>;
-
-    constructor () {
-        console.log ("hi from app.ts in construct");
-        this.partiesxx = Parties.find();
-    }
-}
-
-console.log ("hi from app.ts main 1");
-bootstrap(Socially);
-console.log ("hi from app.ts main 2");
-
-
-
-
-
-
-
-//import 'reflect-metadata';
-//import 'zone.js/dist/zone';
-//import { Component } from '@angular/core';
-//import { bootstrap } from '@angular/platform-browser-dynamic';
-//
-//@Component({
-//    selector: 'app',
-//    templateUrl: 'client/app.html'
-//})
-//class Socially {
-//
-//    parties: Array<Object>;
-//
-//    constructor() {
-//        this.parties = [
-//            {'name': 'Dubstep-Freez Zone',
-//                'description': 'Can we please just for an evening not listen to dubstep.',
-//                'location': 'Palo Alto'
-//            },
-//            {'name': 'All dubstep all the time',
-//                'description': 'Get it on!',
-//                'location': 'Palo Alto'
-//            },
-//            {'name': 'Savage lounging',
-//                'description': 'Leisure suit required. And only fiercest manners.',
-//                'location': 'San Francisco'
-//            }
-//        ];
-//    }
-//}
-//
-//bootstrap(Socially);
+bootstrap(Socially, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
